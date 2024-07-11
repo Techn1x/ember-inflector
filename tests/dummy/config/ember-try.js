63750f23dd5402ad06058d5926ab22e226e1ce43
Brad Overton <bo.inbox+github@protonmail.ch>
2024-07-12 06:05:10 +1000

    Update ember-try.js

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -72,6 +72,7 @@ module.exports = async function () {
         name: 'ember-lts-5.4',
         npm: {
           devDependencies: {
+            '@ember/string': '^4.0.0',
             'ember-source': '~5.4.0',
           },
         },
